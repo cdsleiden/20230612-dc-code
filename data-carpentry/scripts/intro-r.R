23e369ca949c45f1e50788b6a6f8aa5127d902fc
eduardklap <et.klapwijk@gmail.com>
2023-06-12 10:00:49 +0200

    gitautosync update 6

--- data-carpentry/scripts/intro-r.R
+++ data-carpentry/scripts/intro-r.R
@@ -8,7 +8,10 @@ download.file("https://ndownloader.figshare.com/files/11492171",
 # Installing packages
 install.packages("tidyverse")
 
+# R as a calculator
 2 + 16 
+123/56
 
+area_hectares <- 1.0 # assign 1.0 to an object called area_hectares
 
 
