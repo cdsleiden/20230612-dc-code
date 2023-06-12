f5505c5790b14fb660f086b07b59507aa4e91c80
eduardklap <et.klapwijk@gmail.com>
2023-06-12 10:46:09 +0200

    gitautosync update 16

--- data-carpentry/scripts/intro-r.R
+++ data-carpentry/scripts/intro-r.R
@@ -32,6 +32,7 @@ r_area
 # Functions
 round(3.14159) # run the round() function with pi as an argument
 round(pi) # R knows pi
+args(round) # show the arguments of the round() function
 ?round # show Help for the round() function
 round(3.14159, digits = 2) # round pi to 2 decimal points using the digits argument
 round(3.14159, 2) # we can remove the name of the argument when using exactly the same order
