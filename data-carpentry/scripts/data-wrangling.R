0414eb53e8c674bc926bb9552594613350995f50
alwil <alexandra.wilczynska@gmail.com>
2023-06-13 10:42:11 +0200

    gitautosync update 11

--- data-carpentry/scripts/data-wrangling.R
+++ data-carpentry/scripts/data-wrangling.R
@@ -16,6 +16,6 @@ interviews <- read_csv(here("data", "SAFI_clean.csv"), na = "NULL")
 # Select subset of columns 
 select(interviews, village, no_membrs, months_lack_food)
 
-# do the same with subsetting
+# do the same with subseting
 interviews[c("village", "no_membrs", "months_lack_food")]
 
