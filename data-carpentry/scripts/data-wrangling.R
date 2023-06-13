bb8e45ae7f437b677e56fdba81e8bb438e934939
alwil <alexandra.wilczynska@gmail.com>
2023-06-13 10:56:43 +0200

    gitautosync update 26

--- data-carpentry/scripts/data-wrangling.R
+++ data-carpentry/scripts/data-wrangling.R
@@ -45,4 +45,5 @@ interviews_ch <-select(interviews_temp, village:respondent_wall_type)
 
 
 # nest functions 
-
+interviews_ch <-select(filter(interviews, village == "Chirodzo"),
+                       village:respondent_wall_type)
